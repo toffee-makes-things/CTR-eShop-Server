0f62d3abd6cfb981614aeed878c100741f0ca4f6
Lambda <elijah.soule@gmail.com>
2022-04-22 16:54:47 -0700

    php stuff

---
+++ web/config/config.php
@@ -0,0 +1,14 @@
+<?php
+// config.php
+// this holds config info you dont want open to the web
+
+// Database info. 
+// Edit this to your needs
+
+$host = 'localhost';
+$database = 'eshopdb';
+$username = 'eshopadmin';
+$password = '123';
+
+
+?>
--- web/htdocs/ias/services/IdentityAuthenticationSOAP.php
+++ web/htdocs/ias/services/IdentityAuthenticationSOAP.php
@@ -3,6 +3,9 @@
 // Made in PHP 8.1
 // This will handle all SOAP Methods that Nintendo's IAS server does.
 
+// require config. it expects this to be in /var/eshop/config edit to your liking 
+require_once('/var/eshop/config/config.php'); 
+
 
 
 ?>
