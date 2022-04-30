34d9dae92e523c1743d4afbee6cd308c3ddef72a
Lambda <elijah.soule@gmail.com>
2022-04-30 14:44:44 -0700

    eshop.py

---
+++ eshop.py
@@ -0,0 +1,10 @@
+# eshop.py
+# made by lambda
+# this is the base line flask code that deals with setting up everything correctly
+# inspired by WiiLink24's room-server room.py
+
+from flask import flask
+
+
+
+import config
