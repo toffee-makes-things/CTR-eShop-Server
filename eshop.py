e0c6f09c1d23d48cb526dd3411eda92524814b6b
Lambda <elijah.soule@gmail.com>
2022-04-30 15:30:02 -0700

    things

--- eshop.py
+++ eshop.py
@@ -3,8 +3,30 @@
 # this is the base line flask code that deals with setting up everything correctly
 # inspired by WiiLink24's room-server room.py
 
+# imports
+# import flask
 from flask import flask
+# import loginmanager for the admin panel
+from flask_login import LoginManager
 
-
+from models import db, login
 
 import config
+
+
+
+
+
+
+
+
+
+
+
+
+# import routes
+
+import ias
+import ninja
+import ecs
+import samurai
